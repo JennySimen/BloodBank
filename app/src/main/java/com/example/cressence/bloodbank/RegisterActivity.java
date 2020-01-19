97d923b7d8653459b61717ec6277de094e969b8a
Crespy <cressencey@gmail.com>
2020-01-19 20:11:06 +0100

    Removed the gradient from background in the splash activity and changed all the styles values to enable it run on all mobile phones. Adjusted the register activity design. App now works on all devices.

--- app/src/main/java/com/example/cressence/bloodbank/RegisterActivity.java
+++ app/src/main/java/com/example/cressence/bloodbank/RegisterActivity.java
@@ -1,13 +1,28 @@
 package com.example.cressence.bloodbank;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private Button mRegisterBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mRegisterBtn = (Button) findViewById(R.id.regbtn);
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(RegisterActivity.this, UserActivity.class);
+                startActivity(intent1);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/cressence/bloodbank/SplashActivity.java
+++ app/src/main/java/com/example/cressence/bloodbank/SplashActivity.java
@@ -11,7 +11,6 @@ import android.support.v7.app.AppCompatActivity;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-
             setContentView(R.layout.splash);
             startActivity(new Intent(SplashActivity.this, BloodBankActivity.class));
 
